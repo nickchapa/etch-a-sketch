41cd53c2acc89b45b841cdf866dc54fb8a09a319
Nick Chapa <nicky.chapa@hotmail.com>
2026-02-24 07:48:12 -0600

    remove comments

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const sketchDiv = document.querySelector(".sketchDiv");
 
-// for loop to create multiple divs
-// create an array of divs?
 let divArray = [];
 const gridSize = 16;
 
 function newGrid(){
     for(i = 0; i < gridSize; i++){
     divArray[i] = [];
-    //console.log(divArray);
 
     for(j = 0; j < gridSize; j++){
         divArray[i].push(document.createElement("div"));
@@ -19,7 +16,6 @@ function newGrid(){
 
 newGrid();
 
-// remove old grid
 function deleteGrid(){
     for(i = 0; i < gridSize; i++){
         for(j = 0; j < gridSize; j++){
@@ -28,20 +24,9 @@ function deleteGrid(){
     }
 }
 
-// next step: change color on mouse hover
 sketchDiv.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "blue";
 })
-
-// for part 4
-// calculate sketchDiv CSS width and height based on gridSize
-
-// new grid button
-// sends prompt for number of squares per side
-// remove existing grid
-// generate new grid in same container using user input number
-// max of 100
-
 
 const resetBtn = document.querySelector(".resetBtn");
 
